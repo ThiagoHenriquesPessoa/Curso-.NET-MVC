4836613ede6635ea728716d927c855c91071f378
Thiago Henriques <thiagokxl5@gmail.com>
2021-06-16 00:13:37 -0300

    Add novo projeto CursoAPI e usando swash

--- CursoMVC/Models/Categoria.cs
+++ CursoMVC/Models/Categoria.cs
@@ -12,6 +12,6 @@ namespace CursoMVC.Models
         [Display(Name = "Descrição")]
         [Required(ErrorMessage = "O campo descrição é obrigatorio!")]
         public string Descricao { get; set; }
-        public List<Produto> Produtos { get; set; }
+        
     }
 }
